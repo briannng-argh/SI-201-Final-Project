c1215349ebfb9552d4ca513a4618fec566b8819e
Brinanan <khabri999@gmail.com>
2025-12-05 17:16:24 -0500

    API working, loaded json

--- aviation_flights.py
+++ aviation_flights.py
@@ -1,8 +1,28 @@
-def get_flight_data(db_name: str):
-    pass
+import requests
+import sqlite3
+from config import AVIATIONSTACK_API_KEY
+
+def get_flight_data():
+    
+    url = "http://api.aviationstack.com/v1/flights"
+    
+    params = {
+        "access_key": AVIATIONSTACK_API_KEY,
+        "limit": 25
+    }
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    
+    data = response.json()
+    return data
 
 def calculate_travel_time(cur):
     pass
 
 def visualize_flight_time(data):
     pass
+
+
+if __name__ == "__main__":
+    flights = get_flight_data()
+    print(flights) #change later, just making sure it works
